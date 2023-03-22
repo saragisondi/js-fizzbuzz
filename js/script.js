e9b8bcd0aa588ad20e1d074fef63067fd9ef5c72
Sara Gisondi <sara.gisondi@outlook.it>
2023-03-22 23:03:48 +0100

    modify box

--- js/script.js
+++ js/script.js
@@ -2,38 +2,31 @@
 
 let text = '';
 
-//CONTAINER
-const container = document.createElement("div");
-container.classList.add("container");
 
+//CONTAINER
+const container = document.querySelector(".container");
 console.log(container)
 
-//BOX
-const box = document.createElement("div");
-box.classList.add("box");
-
-console.log(box)
-
 //CICLO
-  for(let i=1; i<=100; i++){
-
+for(let i=1; i<=100; i++){
+  
   text = i;
-
+  
   if (!(i % 15)) {
     text = 'fizzBuzz';
-    box.classList.add('yellowgreen')
   } else if (!(i % 5)) {
     text = 'Buzz';
-    box.classList.add('green')
   }else if (!(i % 3)) {
     text = 'Fizz';
-    box.classList.add('yellow')
   }
 
+  //BOX
+  const box = document.createElement("div");
+  box.classList.add("box");
+  console.log(box)
   //BOX DENTRO CONTAINER
   box.append(text)
   container.append(box)
   console.log(text)
 }
-
 
